b46df287c11c0e0ae444a0f90395f4660b4a7476
MigChegga <hafizalghifari2006@gmail.com>
2025-08-14 11:40:08 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -220,5 +220,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('HTTP server listening on port ${PORT}');
+  console.log(`HTTP server listening on port ${PORT}`);
 })
